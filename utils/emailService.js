e48144f7eb047ded6047f4fd2277d36f8102c81a
Nomatter9 <mnomatter9@gmail.com>
2026-08-04 21:08:29 +0200

    fix port number conversion

--- utils/emailService.js
+++ utils/emailService.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  port: 465,
     secure: true,
   auth: {
     user: process.env.MAIL_USERNAME,
